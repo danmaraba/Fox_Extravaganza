41934bd39c50a89ca292ca42eb0830452ba4820a
Daniel Kipruto Maraba <danielkmaraba@gmail.com>
2022-06-24 08:11:14 +0300

    added an eventlistener of DOMContentLoaded to my document

---
+++ index.js
@@ -0,0 +1,68 @@
+document.addEventListener("DOMContentLoaded", () => {
+})
+
+// const url="https://randomfox.ca/floof/"
+
+// const cardTitle = document.getElementById('card-title');
+// const cardImage = document.getElementById('card-image');
+// const likeCount = document.getElementById('like-count');
+// const commentsList = document.getElementById('comments-list');
+// const commentForm = document.getElementById('comment-form');
+// const likeButton = document.getElementById('like-button');
+// const commentButton = document.getElementsByClassName('comment-button');
+// let likes=0;
+
+// document.getElementById('like-button').addEventListener('click',addLikes)
+//     // likes +=1;
+//     // appendLikes();
+
+// document.getElementById('comment-form').addEventListener('submit',addComent)
+
+
+// fetch(url)
+//     .then(res => res.json())
+//     .then(appendElements);
+        
+// function appendElements(data) {
+//     likes=data.likes;
+//     cardImage.src=data.image
+//     appendLikes()
+    
+  
+// }
+
+// function appendLikes(){
+//     document.getElementById("like-count").className=`${ likes} likes`;
+// }
+   
+
+
+// function renderComments(comments){
+//     commentsList.innerHTML='';
+//     comments.map.forEach(renderComment)
+// }
+
+// function renderComment(comment){
+//     console.log(comment)
+//     const li=document.createElement('li')
+//     li.textContent =comment.content;
+//     commentsList.append(li);
+// }
+
+// function addComent(e){
+//     e.preventDefault();
+//     const commentText=e.target.comment.value;
+//     renderComment({content: commentText});
+
+//     e.target.reset();
+// }
+
+// function addLikes(e){
+//     e.preventDefault();
+//     const likes= e.target.previousElementSibling;
+//     likes.textContent = parseInt(likes.textContent)+1
+// }
+
+
+
+
